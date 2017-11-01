d86290797dc6610cf9f937afd45fff4f132a44eb
Jeff Allaire <jeff.allaire@gmail.com>
2017-11-01 16:47:13 -0500

    created database and setting up the PDO

---
+++ libraries/Database.php
@@ -0,0 +1,30 @@
+<?php
+
+class Database {
+	private $host = DB_HOST;
+	private $user = DB_USER;
+	private $pass = DB_PASS;
+	private $dbname =DB_NAME;
+
+	private $dbh;
+    private $error;
+    private $stmt;
+
+    public function__construct() {
+		// Set DSN
+		$dsn = 'mysql:host=' .$this->host . ';dbname=' . $this->dbname;
+		// Set Options
+		$options = array (
+		            PDO::ATTR_PERSISTENT => true,
+					PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION 
+		
+		);
+		
+		
+		
+	}	
+	
+}
+
+
+?>
