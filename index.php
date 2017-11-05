df7bc630c91208df62d58c96653f8583c9c79f91
Jeff Allaire <jeff.allaire@gmail.com>
2017-11-04 20:43:00 -0500

    setup frontpage.php.  Fixed CSS bug.

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php require('core/init.php');
+<?php require('core/init.php');  ?>
+
+<?php 
+    //Get Template and Assign variables
+    
+    $template = new Template('templates/frontpage.php');
+	
+	// Assign vars
+	
+	$template->heading = "This is the Template heading";
+	
+
+    echo $template;   // We can do this because we used the magic method to_string to convert an object to a string.
+
