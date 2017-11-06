c26086c19fd7baea1150f28278d13d914cd86cd4
Jeff Allaire <jeff.allaire@gmail.com>
2017-11-05 19:39:15 -0600

    added comments to the Index.php file

--- index.php
+++ index.php
@@ -10,5 +10,5 @@
 	$template->heading = "This is the Template heading";
 	
 
-    echo $template;   // We can do this because we used the magic method to_string to convert an object to a string.
+    echo $template;   // Can do this because  used the magic method to_string to convert an object to a string.
 
