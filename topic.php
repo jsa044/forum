66bdd563d22c8c83eeadbd6a0482cde4b677b2d7
Jeff Allaire <jeff.allaire@gmail.com>
2017-11-11 11:25:58 -0600

    Started to set up inner pages for topic, topics, create

---
+++ topic.php
@@ -0,0 +1,13 @@
+<?php require('core/init.php');  ?>
+
+<?php 
+    //Get Template and Assign variables
+    
+    $template = new Template('templates/topic.php');
+	
+	// Assign vars
+	
+	$template->heading = "This is the Template heading";
+	
+
+    echo $template;   // Can do this because  used the magic method to_string to convert an object to a string.
