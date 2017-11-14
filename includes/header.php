5e73418d6326b9f68c5884da2ea69b97b7875103
Jeff Allaire <jeff.allaire@gmail.com>
2017-11-14 11:32:29 -0600

    Edited the header file to change static html back to forum

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Welcome To WritingSpace</title>
+    <title>Welcome To TalkingSpace</title>
     <!-- Bootstrap core CSS -->
     <link href="<?php echo BASE_URI;?>templates/css/bootstrap.css" rel="stylesheet">
     <!-- Custom styles for this template -->
@@ -30,13 +30,13 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.php">WritingSpace</a>-->
+          <a class="navbar-brand" href="index.php">TalkingSpace</a>-->
         </div>
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav navbar-right">
             <li class="active"><a href="index.php">Home</a></li>
-            <li><a href="register.php">Create An Account</a></li>
-            <li><a href="create.php">Create a Story</a></li>
+            <li><a href="register.php">Create Account</a></li>
+            <li><a href="create.php">Create Topic</a></li>
           </ul>
         </div>             <!--/.nav-collapse -->
       </div>           <!--.container -->
@@ -49,7 +49,7 @@
 			<div class="col-md-8">
 				<div class="main-col">
 					<div class="block">
-						<h1 class="pull-left">Welcome to WritingSpace</h1>
-						<h4 class="pull-right">A Creative Writing Engine</h4>
+						<h1 class="pull-left">Welcome to TalkingSpace</h1>
+						<h4 class="pull-right">A PHP Forum Engine</h4>
 						<div class="clearfix"></div>
 						<hr>
