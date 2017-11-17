1ec45b0fae8619dc313f65d981ecf218d8325dd5
Jeff Allaire <jeff.allaire@gmail.com>
2017-11-17 14:07:47 -0600

    Worked on making the topic.php dynamic

--- topic.php
+++ topic.php
@@ -1,13 +1,26 @@
 <?php include('core/init.php');  
 
+$topic = new Topic;
+
+//Get the id from the URL
+$topic_id = $_GET['id'];
 
  $template = new Template('templates/post.php');
+ 
+ // Assign Vars
+ 
+ $template->topic = $topic->getTopic($topic_id);
+ $template->replies = $topic->getReplies($topic_id);
+ $template->title = $topic->getTopic($topic_id)->title;
+ 
+ 
+ //Display template
   echo $template;   
 	
 	
 ?>
 
-Wtf
+
     
 	
 	
