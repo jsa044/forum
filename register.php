e5d7e1a3abc57dfb769111104664a76803478703
Jeff Allaire <jeff.allaire@gmail.com>
2017-11-21 08:52:25 -0600

    Set up register page to pull data into db

--- register.php
+++ register.php
@@ -1,13 +1,42 @@
-<?php require('core/init.php');  ?>
+<?php require('core/init.php'); ?>
 
-<?php 
-    //Get Template and Assign variables
-    
-    $template = new Template('templates/register.php');
-	
-	// Assign vars
-	
-	$template->heading = "This is the Template heading";
-	
+<?php
+//Create Topic Object
+$topic = new Topic;
 
-    echo $template;   // Can do this because  used the magic method to_string to convert an object to a string.
+//Create User Object
+$user = new User;
+
+if(isset($_POST['register'])){
+	//Create Data Array
+	$data = array();
+	$data['name'] = $_POST['name'];
+	$data['email'] = $_POST['email'];
+	$data['username'] = $_POST['username'];
+	$data['password'] = md5($_POST['password']);
+	$data['password2'] = md5($_POST['password2']);
+	$data['about'] = $_POST['about'];
+	$data['last_activity'] = date("Y-m-d H:i:s");
+	
+	//Upload Avatar Image
+	if($user->uploadAvatar()){
+		$data['avatar'] = $_FILES["avatar"]["name"];
+	}else{
+		$data['avatar'] = 'noimage.png';
+	}
+	
+	//Register User
+	if($user->register($data)){
+		redirect('index.php', 'You are registered and can now log in', 'success');
+	} else {
+		redirect('index.php', 'Something went wrong with registration', 'error');
+	}
+}
+
+//Get Template & Assign Vars
+$template = new Template('templates/register.php');
+
+//Assign Vars
+
+//Display template
+echo $template;
